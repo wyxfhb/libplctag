76e7d9f70f9f91c3f7a4f79289fc4e5836abab84
Kyle Hayes <kyle.hayes@gmail.com>
2025-12-24 21:29:59 -0800

    The modbus server now checks the unit ID for validity and a match.  Use unit ID 1, not 0.

--- src/tests/scripts/run_modbus_tests.sh
+++ src/tests/scripts/run_modbus_tests.sh
@@ -130,7 +130,7 @@ fi
 
 let TEST++
 echo -n "  Test $TEST: thread stress Modbus... "
-$VALGRIND$TEST_DIR/thread_stress 10 'protocol=modbus-tcp&gateway=127.0.0.1:1502&path=0&elem_count=2&name=hr10' > "$LOG_DIR/${TEST}_modbus_stress_test.log" 2>&1
+$VALGRIND$TEST_DIR/thread_stress 10 'protocol=modbus-tcp&gateway=127.0.0.1:1502&path=1&elem_count=2&name=hr10' > "$LOG_DIR/${TEST}_modbus_stress_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -142,7 +142,7 @@ fi
 
 let TEST++
 echo -n "  Test $TEST: connection stress (multiple connections) Modbus... "
-$VALGRIND$TEST_DIR/test_connection_stress --num-threads=200 --tag='protocol=modbus-tcp&gateway=127.0.0.1:1502&path=0&elem_count=2&name=hr10' > "$LOG_DIR/${TEST}_modbus_connection_stress_test.log" 2>&1
+$VALGRIND$TEST_DIR/test_connection_stress --num-threads=200 --tag='protocol=modbus-tcp&gateway=127.0.0.1:1502&path=1&elem_count=2&name=hr10' > "$LOG_DIR/${TEST}_modbus_connection_stress_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
